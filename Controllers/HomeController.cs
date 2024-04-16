8c57ccf2c49ab55ffe564219209f2b19f98cf828
phguido <pedrosallesguido@gmail.com>
2024-04-15 22:37:11 -0300

    Criaçao do try catch

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using pj01.Models;
+
+namespace pj01.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+            try
+            {
+                throw new Exception();
+                TempData["Sucesso"] = "Deu certo";
+            
+            }
+            catch(Exception ex)
+            {
+                TempData["Erro"] = ex.Message;
+            }
+        return View();
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
